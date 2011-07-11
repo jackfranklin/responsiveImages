27ba9d471b73faa703f65296dc814f1581bd3e4f
Jack Franklin <jack@jackfranklin.co.uk>
2011-07-11 20:38:15 +0100

    Added option to execute the code on resize

--- responsive.images.js
+++ responsive.images.js
@@ -3,21 +3,41 @@
  */
 
 (function(window, document, undefined) {
-    var responsiveImages = function(elem, options) {
+    var responsiveImages = function(elem, options, onResize) {
+        onResize = onResize || false;
         elem = document.getElementById(elem);
-        for(var key in options) {
-            if(key == "else") {
-                elem.src = options["else"];
-                break;
-            } else {
-                var width = parseInt(key);
-                var windowWidth = window.innerWidth;
-                if(windowWidth <= width) {
-                    elem.src = options[key];
+        var pickImage = function(elem) {
+            for(var key in options) {
+                if(key == "else") {
+                    elem.src = options["else"];
                     break;
+                } else {
+                    var width = parseInt(key);
+                    var windowWidth = window.innerWidth;
+                    if(windowWidth <= width) {
+                        elem.src = options[key];
+                        break;
+                    }
                 }
             }
         }
+        pickImage(elem);
+        if(onResize) {
+            addEvent(window, "resize", function() {
+                document.getElementById("bodyWidth").innerHTML = "Width: " + window.innerWidth; 
+                pickImage(elem);
+            });
+        }
     };
     window.responsiveImage = responsiveImages;
+    //helper function for adding events (used for cross browser onresize method)
+    var addEvent = function(elem, type, eventHandle) {
+        if (elem == null || elem == undefined) return;
+        if ( elem.addEventListener ) {
+            elem.addEventListener( type, eventHandle, false );
+        } else if ( elem.attachEvent ) {
+            elem.attachEvent( "on" + type, eventHandle );
+        }
+    };
+        
 })(this, this.document);
