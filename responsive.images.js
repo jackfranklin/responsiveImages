fa86c520bab9d61aa74ca80fd4eee5402703d432
Jack Franklin <jack@jackfranklin.co.uk>
2011-07-11 15:35:16 +0100

    Initial Commit

---
+++ responsive.images.js
@@ -0,0 +1,22 @@
+/*
+ * Written by Jack Franklin
+ */
+
+(function(window, document, undefined) {
+    var responsiveImages = function(elem, options) {
+        elem = document.getElementById(elem);
+        for(var key in options) {
+            if(key == "else") {
+                elem.src = options["else"];
+            } else {
+                var width = parseInt(key);
+                var windowWidth = window.innerWidth;
+                if(windowWidth <= width) {
+                    elem.src = options[key];
+                    break;
+                }
+            }
+        }
+    };
+    window.responsiveImage = responsiveImages;
+})(this, this.document);
